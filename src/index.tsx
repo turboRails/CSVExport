7a91b63a2e40f2059508bf0b1c8faa393a714ba9
Secure Source Initiative <turboRails@users.noreply.github.com>
2025-11-15 12:00:35 +0900

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+  StrictMode,
+} from "react";
+import { createRoot } from "react-dom/client";
+import { AgGridReact } from "ag-grid-react";
+import "./styles.css";
+import {
+  ClientSideRowModelModule,
+  ColDef,
+  ColGroupDef,
+  CsvExportModule,
+  GridApi,
+  GridOptions,
+  ModuleRegistry,
+  NumberEditorModule,
+  TextEditorModule,
+  ValidationModule,
+} from "ag-grid-community";ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  CsvExportModule,
+  NumberEditorModule,
+  TextEditorModule,
+  ...(process.env.NODE_ENV !== "production" ? [ValidationModule] : []),
+]);
+
+const GridExample = () => {
+  const gridRef = useRef<AgGridReact>(null);
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState<any[]>([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ]);
+  const defaultColDef = useMemo<ColDef>(() => {
+    return {
+      editable: true,
+      minWidth: 100,
+      flex: 1,
+    };
+  }, []);
+  const popupParent = useMemo<HTMLElement | null>(() => {
+    return document.body;
+  }, []);
+  const [columnDefs, setColumnDefs] = useState<ColDef[]>([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+
+  const onBtnExport = useCallback(() => {
+    gridRef.current!.api.exportDataAsCsv({fileName: "my.csv"})
+  }, []);
+
+  const onBtnUpdate = useCallback(() => {
+    (document.querySelector("#csvResult") as any).value =
+      gridRef.current!.api.getDataAsCsv();
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <div style={{ margin: "10px 0" }}>
+          <button onClick={onBtnUpdate}>Show CSV export content text</button>
+          <button onClick={onBtnExport}>Download CSV export file</button>
+        </div>
+        <div
+          style={{
+            flex: "1 1 0",
+            position: "relative",
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+          }}
+        >
+          <div id="gridContainer" style={{ flex: "1" }}>
+            <div style={gridStyle}>
+              <AgGridReact
+                ref={gridRef}
+                rowData={rowData}
+                defaultColDef={defaultColDef}
+                suppressExcelExport={true}
+                popupParent={popupParent}
+                columnDefs={columnDefs}
+              />
+            </div>
+          </div>
+          <textarea
+            id="csvResult"
+            style={{ flex: "1" }}
+            placeholder="Click the Show CSV export content button to view exported CSV here"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <GridExample />
+  </StrictMode>,
+);
